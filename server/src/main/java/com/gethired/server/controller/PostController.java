5c22a07441d9522ba75b5431fe7ddd5a4c853c2c
agxmbedi <pavitsinghaps33@gmail.com>
2024-10-23 17:20:21 +0530

    changes

--- server/src/main/java/com/gethired/server/controller/PostController.java
+++ server/src/main/java/com/gethired/server/controller/PostController.java
@@ -7,7 +7,7 @@ import com.gethired.server.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
